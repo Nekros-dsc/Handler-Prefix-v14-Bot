4d4c39021001d8a2d1c34fba9d753476c4bfa4b1
nekrxs <122643715+Nekros-dsc@users.noreply.github.com>
2024-06-17 15:40:36 +0200

    Update messageCreate.js

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -1,9 +1,8 @@
-const config = require('../config.json');
 const Discord = require('discord.js');
 
 module.exports = {
   name: 'messageCreate',
-  async execute(message, bot) {
+  async execute(message, bot, config) {
     try {
       if (!message.guild || message.author.bot) return;
 
